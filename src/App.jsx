5ffb0e4ecf1a3fd7fe1d49ef113fc74da0145a4a
Marianne Mateo <mariannejmb18@gmail.com>
2022-12-30 22:20:04 -0400

    updated!

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <div className="App">
-      <Sidebar />
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut sunt iste a quaerat dicta autem delectus rem libero nihil saepe obcaecati necessitatibus aspernatur dignissimos omnis, ducimus accusamus voluptas provident vel.
     </div>
   )
 }
